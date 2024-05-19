ff1d4e9042276b7a1f15fe42a9ed38d40c7bdb11
miscusos <hhzhz499@gmail.com>
2024-05-19 10:35:00 -0700

    Add files via upload
    rgb

---
+++ rgb.bat
@@ -0,0 +1,55 @@
+@echo off
+echo hello..
+echo you want this text to be colored right? (yes/no)
+:inp
+set /p "input=>> "
+if "%input%"=="" (
+	goto inp
+) else if /i "%input%"=="yes" (
+	goto input2
+) else if /i "%input%"=="no" (
+	echo okay..
+	pause >nul 2>&1
+) else (
+	goto inp
+)
+:input2
+cls
+echo okay..
+echo enter the R from RGB
+:R
+set /p "R=>> "
+if "%R%"=="" (
+	goto R
+)
+:G
+cls
+echo okay..
+echo enter the G from RGB
+:R
+set /p "G=>> "
+if "%G%"=="" (
+	goto G
+)
+:B
+cls
+echo okay..
+echo enter the B from RGB
+:R
+set /p "B=>> "
+if "%B%"=="" (
+	goto B
+)
+set "white=[1;38;2;255;255;255m"
+set "color=[1;38;2;%R%;%G%;%B%m"
+timeout /t 1 >nul
+echo done..
+:txt
+cls
+echo now enter the text you want to display it in the color..
+set /p "text=>> "
+if "%text%"=="" (
+	goto txt
+)
+echo "%color%%text%%white%"
+pause >nul 2>&1
