d8d526c402159a51b477b1cee9fa90f599f8c1c0
miscusos <170189622+miscusos@users.noreply.github.com>
2024-09-19 10:15:25 -0700

    colors.bat updated..

--- colors.bat
+++ colors.bat
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 set /a r=%random% %% 256
 set /a g=%random% %% 256
 set /a b=%random% %% 256
+set "black=[1;38;2;0;0;0m"
 set "color=[1;38;2;%r%;%g%;%b%m"
-echo !color! %r% %g% %b% %r% %g% %b% %r% %g% %b% %r% %g% %b% %r% %g% %b% %r% %g% %b%
+echo !color! %r%%black%;%color%%g%%black%;%color%%b%    %black%black text    %color%example text in the color%back%..
 goto randomColor
